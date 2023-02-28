4b6f50bd74eca0c38482f98d9d2a0cfb26719145
David Riggins <driggins@esri.com>
2023-02-27 17:21:01 -0800

    ClientApp: Add another NuGet pkg. A couple other tweaks for Webix.

--- AspNetCoreAPI.Web/AspNetCoreAPI.Web/ClientApp/src/main.js
+++ AspNetCoreAPI.Web/AspNetCoreAPI.Web/ClientApp/src/main.js
@@ -16,10 +16,8 @@ Object.assign(window, { Vue: { component: app.component } })
 webix.require(['https://desktopqa/libs/webix-forms/scripts/webix-vue.js'], _main_)
 
 async function _main_() {
-  let app = createApp(App)
   app.use(createPinia())
   app.use(router)
 
   app.mount('#app')
 }
-// createApp(App).mount("#app");
